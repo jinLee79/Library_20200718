aee489acacc13cc9139322ca9e834402d354f61e
jslee <etoileettoi@gmail.com>
2020-07-18 11:23:31 +0900

    PhotoView 라이브러리 활용

--- app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
@@ -1,10 +1,17 @@
 package kr.co.tjoeun.library_20200718
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun setUpEvents() {
+        goToPhotoViewBtn.setOnClickListener {
+            val myIntent = Intent(mContext, ProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
     }
 
     override fun setValues() {
---
+++ app/src/main/java/kr/co/tjoeun/library_20200718/ProfilePhotoActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.tjoeun.library_20200718
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilePhotoActivity : BaseActivity() {
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_photo)
+    }
+}
